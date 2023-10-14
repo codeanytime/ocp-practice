f982f565f62469a0042c65ea6d8a54e65584ff75
sonnth <sonnth@nal.vn>
2023-10-14 15:26:11 +0700

    list file extension NIO2

--- ocp/src/main/java/javaNIO2/NIO2Method.java
+++ ocp/src/main/java/javaNIO2/NIO2Method.java
@@ -1,9 +1,8 @@
 package javaNIO2;
 
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.nio.file.*;
+import java.nio.file.attribute.UserPrincipal;
 import java.util.stream.Stream;
 
 /**
@@ -34,8 +33,35 @@ public class NIO2Method {
         Files.move(copy, folderPath.resolve("COPY_NIO2.md"));
         try (Stream<Path> input = Files.walk(folderPath)) {
             input.forEach(path -> {
-                if (path.toFile().isFile())
+                if (path.toFile().isFile()) {
                     System.out.println("AFTER MOVE: " + path.toAbsolutePath().getParent());
+                    try {
+                        System.out.println("OWNER: " + Files.getOwner(path));
+                        FileSystem fileSystem = path.getFileSystem();
+                        System.out.println(fileSystem);
+                        UserPrincipal owner = path.getFileSystem()
+                                .getUserPrincipalLookupService().lookupPrincipalByName("thanhch");
+                        Files.setOwner(path, owner);
+                        System.out.println(Files.getOwner(path));
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                } else {
+                    System.out.println(path.getFileSystem().supportedFileAttributeViews());
+                }
+            });
+        }
+        listFileExtensionInFolder("ocp/src/main/java/javaNIO2", "java");
+    }
+
+    private static void listFileExtensionInFolder(String sourceFolder, String extension) throws IOException {
+        Path path = Paths.get(sourceFolder);
+        if (path.toFile().isFile()) return;
+        try (Stream<Path> input = Files.walk(path)) {
+            input.forEach(p -> {
+                if (p.toString().endsWith(extension)) {
+                    System.out.println(p);
+                }
             });
         }
     }
