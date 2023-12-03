0ae397b44cdbcb8856b2621f6ac9395f06def3be
sonnth <sonnth@nal.vn>
2023-12-03 18:23:45 +0700

    Working with stream -> Q62

--- core.ocp/src/working_with_stream/Q62.java
+++ core.ocp/src/working_with_stream/Q62.java
@@ -1,9 +1,28 @@
-package working_with_stream;/**
+package working_with_stream;
+
+import java.util.OptionalInt;
+
+/**
  * @author thanhch
- *
+ * <p>
  * Date: 03/12/2023
- *
+ * <p>
  * Class: Q62
  */
+// Checked Exception
+class MyException extends Exception {
+    public MyException() {
+    }
+
+    public MyException(String message) {
+        super(message);
+    }
+}
+
 public class Q62 {
+    public static void main(String[] args) throws MyException {
+        OptionalInt opt = OptionalInt.empty();
+        System.out.println(opt.orElseGet(() -> 1));
+        System.out.println(opt.orElseThrow(MyException::new));
+    }
 }
