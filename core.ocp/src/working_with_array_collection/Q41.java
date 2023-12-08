f9e4d1537c0d2dfae9f3214c768eee4e11c0a14b
sonnth <sonnth@nal.vn>
2023-12-08 18:30:35 +0700

    Exception handling, array, collection -> Q41

---
+++ core.ocp/src/working_with_array_collection/Q41.java
@@ -0,0 +1,14 @@
+package working_with_array_collection;
+
+/**
+ * @author thanhch
+ * <p>
+ * Date: 08/12/2023
+ * <p>
+ * Class: Q40
+ */
+public class Q41 {
+    public static void main(String[] args) {
+        System.out.println(Boolean.compare(true, false));
+    }
+}
